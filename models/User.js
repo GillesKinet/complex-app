5b1890f1a3d762aa1f75b3afb6543087043e4789
gilles.kinet <gilleskinet@gmail.com>
2025-02-23 21:19:17 +0100

    avoid duplicate e-mail / username

--- models/User.js
+++ models/User.js
@@ -28,40 +28,66 @@ User.prototype.cleanUp = function () {
 };
 
 User.prototype.validate = function () {
-  if (this.data.username == "") {
-    this.errors.push("You must provide a username.");
-  }
+  return new Promise(async (resolve, reject) => {
+    if (this.data.username == "") {
+      this.errors.push("You must provide a username.");
+    }
 
-  if (
-    this.data.username != "" &&
-    !validator.isAlphanumeric(this.data.username)
-  ) {
-    this.errors.push("Username can only contain letter and numbers");
-  }
+    if (
+      this.data.username != "" &&
+      !validator.isAlphanumeric(this.data.username)
+    ) {
+      this.errors.push("Username can only contain letter and numbers");
+    }
 
-  if (!validator.isEmail(this.data.email)) {
-    this.errors.push("You must provide a valid e-mailadres.");
-  }
+    if (!validator.isEmail(this.data.email)) {
+      this.errors.push("You must provide a valid e-mailadres.");
+    }
 
-  if (this.data.password == "") {
-    this.errors.push("You must provide a password.");
-  }
+    if (this.data.password == "") {
+      this.errors.push("You must provide a password.");
+    }
 
-  if (this.data.password.length > 0 && this.data.password.length < 12) {
-    this.errors.push("Password must be atleast 12 characters");
-  }
+    if (this.data.password.length > 0 && this.data.password.length < 12) {
+      this.errors.push("Password must be atleast 12 characters");
+    }
 
-  if (this.data.password.length > 50) {
-    this.errors.push("Password cannot exceed 50 characters.");
-  }
+    if (this.data.password.length > 50) {
+      this.errors.push("Password cannot exceed 50 characters.");
+    }
 
-  if (this.data.username.length > 0 && this.data.username.length < 3) {
-    this.errors.push("Username must be atleast 3 characters");
-  }
+    if (this.data.username.length > 0 && this.data.username.length < 3) {
+      this.errors.push("Username must be atleast 3 characters");
+    }
 
-  if (this.data.username.length > 30) {
-    this.errors.push("Username cannot exceed 30 characters.");
-  }
+    if (this.data.username.length > 30) {
+      this.errors.push("Username cannot exceed 30 characters.");
+    }
+
+    // Only if user name is valid then check to see if it's already taken
+    if (
+      this.data.username.length > 2 &&
+      this.data.username.length < 31 &&
+      validator.isAlphanumeric(this.data.username)
+    ) {
+      let usernameExists = await usersCollection.findOne({
+        username: this.data.username,
+      });
+      if (usernameExists) {
+        this.errors.push("That username is already taken");
+      }
+    }
+    // Only if email is valid then check to see if it's already taken
+    if (validator.isEmail(this.data.email)) {
+      let emailExists = await usersCollection.findOne({
+        email: this.data.email,
+      });
+      if (emailExists) {
+        this.errors.push("That e-mailaddress is already being used");
+      }
+    }
+    resolve();
+  });
 };
 
 User.prototype.login = function () {
@@ -86,17 +112,23 @@ User.prototype.login = function () {
 };
 
 User.prototype.register = function () {
-  // Step #1: Validate user data
-  this.cleanUp();
-  this.validate(); // so you are pointing to the object created in controller in particular this object => let user = new User(req.body);
+  return new Promise(async (resolve, reject) => {
+    // we use an arrow function since it does not change the value of the this.object!!
+    // Step #1: Validate user data
+    this.cleanUp();
+    await this.validate(); // so you are pointing to the object created in controller in particular this object => let user = new User(req.body);
 
-  // step #2: Only if there are no validations errors, save the user data in the data base
-  if (!this.errors.lenght) {
-    // hash user password
-    let salt = bcrypt.genSaltSync(10);
-    this.data.password = bcrypt.hashSync(this.data.password, salt);
-    usersCollection.insertOne(this.data);
-  }
+    // step #2: Only if there are no validations errors, save the user data in the data base
+    if (!this.errors.length) {
+      // hash user password
+      let salt = bcrypt.genSaltSync(10);
+      this.data.password = bcrypt.hashSync(this.data.password, salt);
+      usersCollection.insertOne(this.data);
+      resolve();
+    } else {
+      reject(this.errors);
+    }
+  });
 }; // a prototype, js will not need to create a copy once for each new object, all objects will have access to this method
 
 module.exports = User;
