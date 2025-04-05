bc90b8fbf849373835939815637d87e24b1f9d4e
gilles.kinet <gilleskinet@gmail.com>
2025-04-05 11:30:06 +0200

    dynamically show the amount of posts, followers, following on profile screen

--- router.js
+++ router.js
@@ -34,6 +34,13 @@ router.get(
   userController.profileFollowersScreen
 );
 
+router.get(
+  "/profile/:username/following",
+  userController.ifUserExists,
+  userController.sharedProfileData,
+  userController.profileFollowingScreen
+);
+
 // post related routes
 
 router.get(
