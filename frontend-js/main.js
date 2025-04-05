805e05623afcf4a94c1d4264b76cb9a4b8dcafa1
gilles.kinet <gilleskinet@gmail.com>
2025-04-05 13:09:58 +0200

    create chat feature

--- frontend-js/main.js
+++ frontend-js/main.js
@@ -1,8 +1,14 @@
 import Search from "./modules/search";
 
+import Chat from "./modules/chat";
+
 new Search();
 
 if (document.querySelector(".header-search-icon")) {
   // this code is for browsers that might be slower
   new Search();
+}
+
+if (document.querySelector("#chat-wrapper")) {
+  new Chat();
 }
