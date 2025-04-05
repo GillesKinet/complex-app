52d303828c42d9ee44b0648b1c4a2f1da05d763c
gilles.kinet <gilleskinet@gmail.com>
2025-04-05 13:22:24 +0200

    setup IO part I

--- app.js
+++ app.js
@@ -76,4 +76,12 @@ app.set("view engine", "ejs"); // npm install ejs to install
 
 app.use("/", router);
 
-module.exports = app;
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+io.on("connection", function () {
+  console.log("A new user connected");
+});
+
+module.exports = server;
--- frontend-js/modules/chat.js
+++ frontend-js/modules/chat.js
@@ -28,7 +28,7 @@ export default class Chat {
   }
 
   openConnection() {
-    alert("Opening a connection");
+    this.socket = io();
   }
 
   injectHTML() {
