8e25c6644c48f87e4a1bac9613413a08a6a806ba
gilles.kinet <gilleskinet@gmail.com>
2025-04-13 11:09:14 +0200

    show relevant user names and avatars

--- app.js
+++ app.js
@@ -77,13 +77,24 @@ app.set("view engine", "ejs"); // npm install ejs to install
 app.use("/", router);
 
 const server = require("http").createServer(app);
-
 const io = require("socket.io")(server);
 
+io.use(function (socket, next) {
+  sessionOptions(socket.request, socket.request.res || {}, next);
+});
+
 io.on("connection", function (socket) {
-  socket.on("chatMessageFromBrowser", function (data) {
-    io.emit("chatMessageFromServer", { message: data.message });
-  });
+  if (socket.request.session.user) {
+    // check is uers is logged in
+    let user = socket.request.session.user;
+    socket.on("chatMessageFromBrowser", function (data) {
+      io.emit("chatMessageFromServer", {
+        message: data.message,
+        username: user.username,
+        avatar: user.avatar,
+      });
+    });
+  }
 });
 
 module.exports = server;
